327faaf86e054612504c3125deacb80c2aaeaa67
Documenter.jl <documenter@juliadocs.github.io>
2021-10-19 11:52:54 +0000

    build based on 2311046

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = bigleaf","category":"page"},{"location":"#bigleaf","page":"Home","title":"bigleaf","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for bigleaf.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [bigleaf]","category":"page"}]
+}
