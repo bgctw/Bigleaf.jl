83140cc7c2564c149501ea8ad566be3e65ff47f5
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2021-10-22 19:40:29 +0200

    remove vec_tuple_to_DataFrame
    actually not used

--- src/util.jl
+++ src/util.jl
@@ -1,12 +1,20 @@
 export toDataFrame
 
+"""
+    toDataFrame(x::AbstractVector{<:SLArray}) 
+
+Convert an Vector{SLVector} to DataFrame.
+
+Such objects are returned when broadcasting on a function
+that retuns an SLVector.
+"""
 function toDataFrame(x::AbstractVector{<:SLArray}) 
     names = collect(keys(first(x)))
     df = DataFrame(Tables.table(VectorOfArray(x)'))
     rename!(df, names)
 end
 
-function vec_tuple_to_DataFrame(data; names = string.(keys(first(data))))
-    DataFrame(collect(map(idx -> getindex.(data, idx), eachindex(collect(first(data))))), collect(names))
-end
+# function vec_tuple_to_DataFrame(data; names = string.(keys(first(data))))
+#     DataFrame(collect(map(idx -> getindex.(data, idx), eachindex(collect(first(data))))), collect(names))
+# end
 
