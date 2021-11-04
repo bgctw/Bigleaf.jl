fd3875d9a7b9fd1feb9b69a08cd5ea703a366e3f
Thomas Wutzler <twutz@bgc-jena.mpg.de>
2021-11-04 10:50:08 +0100

    test fixing DocTest error
    by using DataFrames in make.jl
    in addition to using DataFrames in DocTestSetup

--- docs/make.jl
+++ docs/make.jl
@@ -1,16 +1,17 @@
 using Bigleaf
-using Documenter, Latexify
+using Documenter, Latexify, DataFrames
 
 # allow plot to work without display
 # https://discourse.julialang.org/t/generation-of-documentation-fails-qt-qpa-xcb-could-not-connect-to-display/60988/2
 ENV["GKSwstype"] = "100"
 
-DocMeta.setdocmeta!(Bigleaf, :DocTestSetup, :(using Bigleaf, Latexify, DataFrames); recursive=true, warn=false)
+DocMeta.setdocmeta!(Bigleaf, :DocTestSetup, :(using Bigleaf, Latexify, DataFrames); 
+    recursive=true, warn=false)
 doctest(Bigleaf, manual = false)
 
 makedocs(;
     modules=[Bigleaf],
-    authors="Thomas Wutzler <twutz@bgc-jena.mpg.de>. Jürgen Knauer <Juergen.Knauer@csiro.au> and contributors",
+    authors="Thomas Wutzler <twutz@bgc-jena.mpg.de>, Jürgen Knauer <Juergen.Knauer@csiro.au> and contributors",
     repo="https://github.com/bgctw/Bigleaf.jl/blob/{commit}{path}#{line}",
     sitename="Bigleaf.jl",
     doctestfilters=[r".*Info.*"],
